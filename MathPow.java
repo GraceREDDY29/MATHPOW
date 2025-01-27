a04ec011eaef6823b6c6a17a574c8cf12063b43d
GraceREDDY29 <gundampalligrace@gmail.com>
2025-01-27 23:14:06 +0530

    Add files via upload

---
+++ MathPow.java
@@ -0,0 +1,8 @@
+public class MathPow {
+         public static void main(String[] args) {
+            int p = (int) Math.pow(8, 20);
+            System.out.printf("8 to the power of 20 is %d", p);
+        }
+    }
+    
+
